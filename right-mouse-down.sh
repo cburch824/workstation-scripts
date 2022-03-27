52c04e4df4ee5073399effef62f314172b52895f
chrisburch <cburch@appliedsystems.com>
2022-03-27 16:05:10 -0500

    Write right-mouse-down.sh coordinates in x, y format

--- right-mouse-down.sh
+++ right-mouse-down.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+regex="x:([0-9]*)\sy:([0-9]*)*"
 while true; 
 do 
 if [[ $(xinput query-state 19 | grep 'button\[3\]=down') ]]
 then
-echo $(xdotool getmouselocation)
+xyValues=$(xdotool getmouselocation)
+
+if [[ $xyValues =~ $regex ]]
+then
+  x="${BASH_REMATCH[1]}"
+  y="${BASH_REMATCH[2]}"
+  echo "$x, $y"
+fi
 fi
 sleep 0.5; 
 done
